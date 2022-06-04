d80bca78307c4eba35f5b05e2cf42bae2a47768c
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-04 14:29:33 +0100

    user createHandling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import logger from './utils/logger';
 import 'dotenv/config';
+import userRoutes from './modules/user/user.routes';
+import { userSchemas } from './modules/user/user.schemas';
 
 const port = process.env.PORT;
 
@@ -10,7 +12,13 @@ server.get('/healthcheck', async function () {
     return { status: 'OK' };
 });
 
+server.register(userRoutes, { prefix: 'api/users' });
+
 async function main() {
+    for (const schema of userSchemas) {
+        server.addSchema(schema);
+    }
+
     try {
         await server.listen(port, '0.0.0.0');
         logger.info(`Server ready at http://localhost:${port}`);
