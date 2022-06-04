fed0c1aea713601b5a59599fb8b5337a93ceae88
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-04 18:32:54 +0100

    Setup jwt

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,42 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import fjwt from '@fastify/jwt';
+import swagger from 'fastify-swagger';
+import { withRefResolver } from 'fastify-zod';
 import logger from './utils/logger';
 import 'dotenv/config';
 import userRoutes from './modules/user/user.routes';
 import { userSchemas } from './modules/user/user.schemas';
 
-const port = process.env.PORT;
+const port = process.env.PORT!;
+const jwtSecret = process.env.JWTSECRET!;
 
 export const server = Fastify();
+
+server.register(fjwt, {
+    secret: jwtSecret,
+});
+
+server.decorate(
+    'authenticate',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+        try {
+            await request.jwtVerify();
+        } catch (e) {
+            return reply.send(e);
+        }
+    },
+);
 
 server.get('/healthcheck', async function () {
     return { status: 'OK' };
 });
 
-server.register(userRoutes, { prefix: 'api/users' });
-
 async function main() {
     for (const schema of userSchemas) {
         server.addSchema(schema);
     }
+
+    server.register(userRoutes, { prefix: 'api/users' });
 
     try {
         await server.listen(port, '0.0.0.0');
