45a9755dc49d70efa38d62f1dcd42c74e4ee48a0
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-04 02:53:00 +0100

    added the layout to the main app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,21 @@ import Rdvs from './pages/Rdvs';
 import Salle from './pages/Salle';
 import Patients from './pages/Patients';
 import Patient from './pages/Patient';
+import Layout from './components/Layout';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Auth />} />
-                <Route path="/Acceuil" element={<Acceuil />} />
-                <Route path="/Patients" element={<Patients />} />
-                <Route path="/Patients/:userId" element={<Patient />} />
-                <Route path="/Rendez-vous" element={<Rdvs />} />
-                <Route path="/Salle" element={<Salle />} />
+
+                <Route element={<Layout />}>
+                    <Route path="/Acceuil" element={<Acceuil />} />
+                    <Route path="/Patients" element={<Patients />} />
+                    <Route path="/Patients/:userId" element={<Patient />} />
+                    <Route path="/Rendez-vous" element={<Rdvs />} />
+                    <Route path="/Salle" element={<Salle />} />
+                </Route>
             </Routes>
         </Router>
     );
