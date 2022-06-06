da5fa7e1a9b4f66e5ab988d5d46341c3ce38782a
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-06 03:08:23 +0100

    making acceuil page

--- client/src/pages/Acceuil/index.tsx
+++ client/src/pages/Acceuil/index.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import RdvWidget from '../../components/Acceuil/RdvWidget';
+import style from './style.module.css';
 
 export default function Acceuil() {
-    return <div>Acceuil Page</div>;
+    return (
+        <div className={style.Acceuil}>
+            <div className={style.title}>Acceuil</div>
+            <main>
+                <div className={style.widgets}>
+                    <RdvWidget />
+                </div>
+                <div className={style.table}></div>
+            </main>
+        </div>
+    );
 }
