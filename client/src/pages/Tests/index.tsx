a04d3c10f776221132a89ce276bf67c55fbc7c6f
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-10 18:38:47 +0100

    Added actions to the tests grid

--- client/src/pages/Tests/index.tsx
+++ client/src/pages/Tests/index.tsx
@@ -1,48 +1,59 @@
 import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 import style from './style.module.css';
 import newTestIcon from '../../assets/icons/Tests/newTest.svg';
+import TestsActions from "../../components/GridActions/TestsActions";
 
 export default function Tests() {
     const rows: GridRowsProp = [
         {
             id: 1,
-            nom: 'BENNOUNA',
-            prenom: 'Mehdi',
+            nom: "BENNOUNA",
+            prenom: "Mehdi",
             age: 22,
-            sexe: 'Homme',
-            telephone: '0562099072',
-            motif: 'Too smart',
-            nRdv: '22/10/2000',
+            sexe: "Homme",
+            telephone: "0562099072",
+            motif: "Too smart",
+            nRdv: "22/10/2000",
         },
         {
             id: 2,
-            nom: 'BENNOUNA',
-            prenom: 'Mehdi',
+            nom: "BENNOUNA",
+            prenom: "Mehdi",
             age: 22,
-            sexe: 'Homme',
-            telephone: '0562099072',
-            motif: 'Too smart',
-            nRdv: '22/10/2000',
+            sexe: "Homme",
+            telephone: "0562099072",
+            motif: "Too smart",
+            nRdv: "22/10/2000",
         },
         {
             id: 3,
-            nom: 'BENNOUNA',
-            prenom: 'Mehdi',
+            nom: "BENNOUNA",
+            prenom: "Mehdi",
             age: 22,
-            sexe: 'Homme',
-            telephone: '0562099072',
-            motif: 'Too smart',
-            nRdv: '22/10/2000',
+            sexe: "Homme",
+            telephone: "0562099072",
+            motif: "Too smart",
+            nRdv: "22/10/2000",
         },
     ];
 
     const columns: GridColDef[] = [
-        { field: 'nom', headerName: 'Nom', width: 200, type: 'date' },
-        { field: 'prenom', headerName: 'Prenom', width: 230 },
-        { field: 'telephone', headerName: 'Telephone', width: 200 },
-        { field: 'age', headerName: 'Age', width: 110 },
-        { field: 'motif', headerName: 'Motif', width: 150 },
-        { field: 'heure', headerName: 'Heure', width: 150 },
+        { field: "nom", headerName: "Nom", width: 200, type: "date" },
+        { field: "prenom", headerName: "Prenom", width: 230 },
+        { field: "telephone", headerName: "Telephone", width: 200 },
+        { field: "age", headerName: "Age", width: 110 },
+        { field: "motif", headerName: "Motif", width: 150 },
+        { field: "heure", headerName: "Heure", width: 150 },
+        {
+            field: "actions",
+            headerName: "Actions",
+            type: "actions",
+            width: 200,
+            align: "left",
+            renderCell: (test) => {
+                return <TestsActions />;
+            },
+        },
     ];
 
     return (
@@ -60,12 +71,15 @@ export default function Tests() {
                         hideFooterSelectedRowCount
                         pageSize={11}
                         sx={{
-                            border: 'none',
-                            fontStyle: 'normal',
-                            fontWeight: '500',
-                            fontSize: '16px',
-                            lineHeight: '19px',
-                            color: '#455560',
+                            border: "none",
+                            fontStyle: "normal",
+                            fontWeight: "500",
+                            fontSize: "16px",
+                            lineHeight: "19px",
+                            color: "#455560",
+                            "& .MuiDataGrid-row:hover .invis": {
+                                visibility: "visible",
+                            },
                         }}
                     />
                 </div>
