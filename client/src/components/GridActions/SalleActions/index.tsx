74b471c8c3f9348e3ddf42180560f911686b249e
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-10 20:27:28 +0100

    added grid actions to Salle page

---
+++ client/src/components/GridActions/SalleActions/index.tsx
@@ -0,0 +1,21 @@
+import style from "./style.module.css";
+
+import EditIcon from "../../../assets/icons/GridActions/EditIcon.svg";
+import OpenIcon from "../../../assets/icons/GridActions/OpenIcon.svg";
+import DeleteIcon from "../../../assets/icons/GridActions/DeleteIcon.svg";
+
+export default function SalleActions() {
+    const myStyle = `.invis {
+		visibility : hidden;
+	}`;
+    return (
+        <>
+            <style>{myStyle}</style>
+            <div className={`${style.SalleActions} invis`}>
+                <img src={EditIcon} alt="" />
+                <img src={OpenIcon} alt="" />
+                <img src={DeleteIcon} alt="" />
+            </div>
+        </>
+    );
+}
