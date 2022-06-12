d85e31cf33c7b621e606e004d18af5a88c11e107
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-12 09:38:07 +0100

    remake mesures widget

--- client/src/components/Patient/MesuresWidget/index.tsx
+++ client/src/components/Patient/MesuresWidget/index.tsx
@@ -1,19 +1,21 @@
 import style from "./style.module.css";
+
 export default function MesuresWidget() {
     return (
         <div className={style.MesuresWidget}>
             <h1>Mesures</h1>
-            <div className={style.content}>
-                <h2>
-                    Poids : <span>74kg</span>
-                </h2>
-                <h2>
-                    Tension : <span>44</span>
-                </h2>
-                <h2>
-                    somethign : <span>ston</span>
-                </h2>
-            </div>
+            <h2>
+                Poids : <span>74kg</span>
+            </h2>
+            <h2>
+                Tension : <span>tson</span>
+            </h2>
+            <h2>
+                Age : <span>22</span>
+            </h2>
+            <h2>
+                okay : <span>stosnr</span>
+            </h2>
         </div>
     );
 }
