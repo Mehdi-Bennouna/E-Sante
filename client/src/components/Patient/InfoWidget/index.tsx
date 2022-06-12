8882b26bfa5d833e20f6d4d808d31cda7c6ac566
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-12 09:37:50 +0100

    remake info widget

--- client/src/components/Patient/InfoWidget/index.tsx
+++ client/src/components/Patient/InfoWidget/index.tsx
@@ -4,43 +4,45 @@ export default function InfoWidget() {
     return (
         <div className={style.InfoWidget}>
             <h1>Informations & Contacts</h1>
-            <div className={style.line}>
-                <h2>
-                    Nom : <span>BENNOUNA</span>
-                </h2>
-                <h2>
-                    Prenom : <span>Mehdi</span>
-                </h2>
-            </div>
+            <div className={style.info}>
+                <div className={style.line}>
+                    <h2>
+                        Nom : <span>BENNOUNA</span>
+                    </h2>
 
-            <div className={style.line}>
-                <h2>
-                    Age : <span>22</span>
-                </h2>
-                <h2>
-                    Poid : <span>74kg</span>
-                </h2>
-                <h2>
-                    Sexe : <span>Homme</span>
-                </h2>
-            </div>
+                    <h2>
+                        Prenom : <span>Mehdi</span>
+                    </h2>
+                </div>
 
+                <div className={style.line}>
+                    <h2>
+                        Date de naissance : <span>22/10/2000</span>
+                    </h2>
+                    <h2>
+                        Poids : <span>74kg</span>
+                    </h2>
+                    <h2>
+                        Sexe : <span>Homme</span>
+                    </h2>
+                </div>
+            </div>
             <hr />
-
-            <div className={style.line}>
-                <h2>
-                    Telephone : <span>0562099072</span>
-                </h2>
-                <h2>
-                    E-mail : <span>mehdi.m.bennouna@gmail.com</span>
-                </h2>
-            </div>
-
-            <div className={style.line}>
-                <h2>
-                    Addresse :{" "}
-                    <span>22, Hammou Boutlilis, Ain El Bya, Oran</span>
-                </h2>
+            <div className={style.contacts}>
+                <div className={style.line}>
+                    <h2>
+                        Téléphone : <span>0562099072</span>
+                    </h2>
+                    <h2>
+                        E-Mail : <span>mehdi.m.bennouna@gmail.com</span>
+                    </h2>
+                </div>
+                <div className={style.line}>
+                    <h2>
+                        Addresse :{" "}
+                        <span>20 bd, Hammou Boutlilis, Ain El Bya, Oran </span>
+                    </h2>
+                </div>
             </div>
         </div>
     );
