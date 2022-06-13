5b6f688af353e317177184e9cd0ee6189fd2d0a0
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-13 23:03:11 +0100

    base login redux stuff

--- client/src/components/SignInCard/index.tsx
+++ client/src/components/SignInCard/index.tsx
@@ -1,8 +1,32 @@
-import style from './style.module.css';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../features/userSlice";
+import style from "./style.module.css";
 
 export default function SignInCard() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        dispatch(
+            login({
+                username: username,
+                password: password,
+                loggedIn: true,
+            }),
+        );
+
+        navigate({ pathname: "/Acceuil" });
+    };
+
     return (
-        <form action="" className={style.SignInCard}>
+        <form className={style.SignInCard} onSubmit={(e) => handleSubmit(e)}>
             <div className={style.headers}>
                 <h1>Connexion</h1>
                 <h2>
@@ -11,8 +35,19 @@ export default function SignInCard() {
             </div>
 
             <div className={style.inputs}>
-                <input type="text" placeholder="Nom d' utilisateur" />
-                <input type="password" placeholder="Mot de passe" />
+                <input
+                    type="text"
+                    placeholder="Nom d' utilisateur"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Mot de passe"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <h3>Utilisateur ou mot de passe invalide</h3>
             </div>
 
