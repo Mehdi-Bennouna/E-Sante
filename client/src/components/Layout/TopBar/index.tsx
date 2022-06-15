4b89a797a7c40a6fa881133eefa5dd96adf89393
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-15 03:40:42 +0100

    added logout button

--- client/src/components/Layout/TopBar/index.tsx
+++ client/src/components/Layout/TopBar/index.tsx
@@ -1,8 +1,26 @@
-import style from './style.module.css';
-import userIcon from '../../../assets/icons/TopBar/User.svg';
-import searchIcon from '../../../assets/icons/TopBar/Search.svg';
+import style from "./style.module.css";
+import userIcon from "../../../assets/icons/TopBar/User.svg";
+import searchIcon from "../../../assets/icons/TopBar/Search.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../../../features/userSlice";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function TopBar() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [active, setActive] = useState(false);
+    const user = useSelector(selectUser);
+
+    const handleLogout = async (e: any) => {
+        e.preventDefault();
+        dispatch(logout());
+        navigate("/");
+    };
+
+    const name = `${user.prenom} ${user.nom}`;
+
     return (
         <div className={style.TopBar}>
             <div className={style.search}>
@@ -10,9 +28,24 @@ export default function TopBar() {
                 <img src={searchIcon} alt="" />
             </div>
 
-            <div className={style.user}>
-                <img src={userIcon} alt="" />
-                <h1>BENNOUNA Mehdi</h1>
+            <div className={style.logout}>
+                <div
+                    className={style.user}
+                    onClick={() => {
+                        setActive(!active);
+                    }}
+                >
+                    <img src={userIcon} alt="" />
+                    <h1>{name}</h1>
+                </div>
+                <div
+                    className={`${style.logout_button} ${
+                        active ? "" : style.invis
+                    }`}
+                    onClick={handleLogout}
+                >
+                    Verrouiller
+                </div>
             </div>
         </div>
     );
