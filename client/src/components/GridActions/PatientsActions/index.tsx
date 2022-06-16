c68273bf5d8a150dc9b49345ecdfe0c9136a946c
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-16 14:20:51 +0100

    can render patients and delete

--- client/src/components/GridActions/PatientsActions/index.tsx
+++ client/src/components/GridActions/PatientsActions/index.tsx
@@ -3,18 +3,41 @@ import style from "./style.module.css";
 import EditIcon from "../../../assets/icons/GridActions/EditIcon.svg";
 import OpenIcon from "../../../assets/icons/GridActions/OpenIcon.svg";
 import DeleteIcon from "../../../assets/icons/GridActions/DeleteIcon.svg";
+import axios from "axios";
 
-export default function PatientsActions() {
+export default function PatientsActions(props: any) {
     const myStyle = `.invis {
 		visibility : hidden;
 	}`;
+
+    const handleEdit = () => {};
+
+    const handleOpen = () => {
+        console.log(props.row);
+    };
+
+    const handleDelete = () => {
+        axios.delete("http://localhost:3001/api/patients", {
+            data: { nom: props.row.nom, prenom: props.row.prenom },
+        });
+        props.setData(
+            props.data.filter((element: any) => {
+                if (element.id === props.row.id) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }),
+        );
+    };
+
     return (
         <>
             <style>{myStyle}</style>
             <div className={`${style.PatientsActions} invis`}>
-                <img src={EditIcon} alt="" />
-                <img src={OpenIcon} alt="" />
-                <img src={DeleteIcon} alt="" />
+                <img src={EditIcon} alt="" onClick={handleEdit} />
+                <img src={OpenIcon} alt="" onClick={handleOpen} />
+                <img src={DeleteIcon} alt="" onClick={handleDelete} />
             </div>
         </>
     );
--- client/src/pages/Patients/index.tsx
+++ client/src/pages/Patients/index.tsx
@@ -1,143 +1,45 @@
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { DataGrid, frFR, GridColDef } from "@mui/x-data-grid";
 import style from "./style.module.css";
 import newFolderIcon from "../../assets/icons/Patients/newFolder.svg";
 import PatientsActions from "../../components/GridActions/PatientsActions";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 export default function Patients() {
     const navigate = useNavigate();
-    const rows: GridRowsProp = [
-        {
-            id: 1,
-            nom: "ATTABI",
-            prenom: "Dahlia Melissa",
-            age: 22,
-            sexe: "Femme",
-            telephone: "0629226015",
-            nRdv: "22/06/2022",
-            pRdv: "22/05/2022",
-        },
-        {
-            id: 2,
-            nom: "SLIMANI",
-            prenom: "Ismahane",
-            age: 22,
-            sexe: "Femme",
-            telephone: "0682821937",
-            nRdv: "22/07/2022",
-            pRdv: "22/04/2022",
-        },
-        {
-            id: 3,
-            nom: "ROUABAH",
-            prenom: "Naila",
-            age: 22,
-            sexe: "Femme",
-            telephone: "0778314973",
-            nRdv: "-",
-            pRdv: "02/06/2022",
-        },
-        {
-            id: 5,
-            nom: "MADADI",
-            prenom: "Youcef",
-            age: 22,
-            sexe: "Homme",
-            telephone: "0759995028",
-            nRdv: "-",
-            pRdv: "12/06/2022",
-        },
-        {
-            id: 6,
-            nom: "KHOUMERI",
-            prenom: "Lyes",
-            age: 22,
-            sexe: "Homme",
-            telephone: "0758526842",
-            nRdv: "21/06/2022",
-            pRdv: "21/05/2022",
-        },
-        {
-            id: 7,
-            nom: "YALA",
-            prenom: "Manel",
-            age: 22,
-            sexe: "Femme",
-            telephone: "0573055800",
-            nRdv: "14/06/2022",
-            pRdv: "07/04/2022",
-        },
-        {
-            id: 8,
-            nom: "GHENAIET",
-            prenom: "Walid",
-            age: 22,
-            sexe: "Homme",
-            telephone: "0508753106",
-            nRdv: "29/06/2022",
-            pRdv: "01/06/2022",
-        },
-        {
-            id: 9,
-            nom: "BENKHELIFA",
-            prenom: "Abdelghafour",
-            age: 21,
-            sexe: "Homme",
-            telephone: "0528162035",
-            nRdv: "-",
-            pRdv: "02/06/2022",
-        },
-        {
-            id: 10,
-            nom: "BRAHIM-DJELLOUL",
-            prenom: "Sami",
-            age: 22,
-            sexe: "Homme",
-            telephone: "0739500698",
-            nRdv: "01/07/2022",
-            pRdv: "02/04/2022",
-        },
-        {
-            id: 11,
-            nom: "NASS",
-            prenom: "Manel",
-            age: 22,
-            sexe: "Femme",
-            telephone: "0680374023",
-            nRdv: "-",
-            pRdv: "22/06/2022",
-        },
-        {
-            id: 12,
-            nom: "CHIKHI",
-            prenom: "Sofiane",
-            age: 22,
-            sexe: "Homme",
-            telephone: "777928911",
-            nRdv: "-",
-            pRdv: "22/06/2022",
-        },
-        {
-            id: 13,
-            nom: "YENNEK",
-            prenom: "Kamel",
-            age: 22,
-            sexe: "Homme",
-            telephone: "0664823147",
-            nRdv: "22/10/2022",
-            pRdv: "22/10/2022",
-        },
-        {
-            id: 14,
-            nom: "ATTABI",
-            prenom: "Lilia Dina",
-            age: 22,
-            sexe: "Femme",
-            telephone: "0783395880",
-            nRdv: "22/10/2022",
-            pRdv: "22/10/2022",
-        },
-    ];
+    const [data, setData] = useState([]);
+
+    const theme = createTheme({}, frFR);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/api/patients").then((response) => {
+            setData(
+                response.data.map((element: any, index: any) => {
+                    return {
+                        id: index + 1,
+                        nom: element.nom,
+                        prenom: element.prenom,
+                        age: "the age",
+                        sexe: element.sexe,
+                        telephone: element.tel,
+                        nRdv: element.RendezVous[0]
+                            ? moment(element.RendezVous[0].heure).format(
+                                  "yyyy/MM/DD",
+                              )
+                            : "-",
+                        pRdv: element.RendezVous[1]
+                            ? moment(element.RendezVous[1].heure).format(
+                                  "yyyy/MM/DD",
+                              )
+                            : "-",
+                    };
+                }),
+            );
+        });
+    }, []);
 
     const columns: GridColDef[] = [
         { field: "nom", headerName: "Nom", width: 200 },
@@ -153,8 +55,14 @@ export default function Patients() {
             type: "actions",
             width: 200,
             align: "left",
-            renderCell: (test) => {
-                return <PatientsActions />;
+            renderCell: (params) => {
+                return (
+                    <PatientsActions
+                        row={params.row}
+                        data={data}
+                        setData={setData}
+                    />
+                );
             },
         },
     ];
@@ -164,44 +72,45 @@ export default function Patients() {
     };
 
     return (
-        <div className={style.Patients}>
-            <div className={style.title}>Patients</div>
-            <main>
-                <div
-                    className={style.add_patient}
-                    onClick={handleCreatePatient}
-                >
-                    <img src={newFolderIcon} alt="" />
-                    <h3>Créer dossier patient</h3>
-                </div>
-                <div className={style.table}>
-                    <DataGrid
-                        rows={rows}
-                        columns={columns}
-                        hideFooterSelectedRowCount
-                        pageSize={11}
-                        disableSelectionOnClick
-                        sx={{
-                            border: "none",
-                            fontStyle: "normal",
-                            fontWeight: "500",
-                            fontSize: "16px",
-                            lineHeight: "19px",
-                            color: "#455560",
-                            "& .MuiDataGrid-row:hover .invis": {
-                                visibility: "visible",
-                                backgroundColor: "#FAFAFB",
-                            },
-                            "& .MuiDataGrid-row:hover": {
-                                backgroundColor: "#FAFAFB",
-                            },
-                            "&.MuiDataGrid-root .MuiDataGrid-cell:focus": {
-                                outline: "none",
-                            },
-                        }}
-                    />
-                </div>
-            </main>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className={style.Patients}>
+                <div className={style.title}>Patients</div>
+                <main>
+                    <div
+                        className={style.add_patient}
+                        onClick={handleCreatePatient}
+                    >
+                        <img src={newFolderIcon} alt="" />
+                        <h3>Créer dossier patient</h3>
+                    </div>
+                    <div className={style.table}>
+                        <DataGrid
+                            rows={data}
+                            columns={columns}
+                            hideFooterSelectedRowCount
+                            disableSelectionOnClick
+                            sx={{
+                                border: "none",
+                                fontStyle: "normal",
+                                fontWeight: "500",
+                                fontSize: "16px",
+                                lineHeight: "19px",
+                                color: "#455560",
+                                "& .MuiDataGrid-row:hover .invis": {
+                                    visibility: "visible",
+                                    backgroundColor: "#FAFAFB",
+                                },
+                                "& .MuiDataGrid-row:hover": {
+                                    backgroundColor: "#FAFAFB",
+                                },
+                                "&.MuiDataGrid-root .MuiDataGrid-cell:focus": {
+                                    outline: "none",
+                                },
+                            }}
+                        />
+                    </div>
+                </main>
+            </div>
+        </ThemeProvider>
     );
 }
