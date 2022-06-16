fdbda7b5c1662c0c428340f5a38b91264f81f82d
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-16 02:38:46 +0100

    rendez vous page not hardcoded anymore

--- client/src/pages/Rdvs/index.tsx
+++ client/src/pages/Rdvs/index.tsx
@@ -1,47 +1,59 @@
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
-import newRdvIcon from '../../assets/icons/Rendez_vous/newRdv.svg';
+import { TextField } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+
+import axios from "axios";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import newRdvIcon from "../../assets/icons/Rendez_vous/newRdv.svg";
 import RdvsActions from "../../components/GridActions/RdvsActions";
 import style from "./style.module.css";
 
+function formatDate(date: Date | string) {
+    return moment(date).format("yyyy/MM/DD");
+}
+
 export default function Rdvs() {
-    const rows: GridRowsProp = [
-        {
-            id: 1,
-            nom: "BENKHELIFA",
-            prenom: "Abdelghafour",
-            age: 21,
-            sexe: "Homme",
-            telephone: "0562099072",
-            motif: "-",
-            heure: "8:30",
-        },
-        {
-            id: 2,
-            nom: "ROUABAH",
-            prenom: "Naila",
-            age: 21,
-            sexe: "Femme",
-            telephone: "0561873072",
-            motif: "-",
-            heure: "9:10",
-        },
-        {
-            id: 3,
-            nom: "CHIKHI",
-            prenom: "Sofiane",
-            age: 22,
-            sexe: "Homme",
-            telephone: "0562099072",
-            motif: "-",
-            heure: "9:40",
-        },
-    ];
+    const today = formatDate(new Date());
+
+    const [data, setData] = useState([]);
+    const [date, setDate] = useState(today);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/api/rdvs/").then((response) => {
+            setData(
+                response.data
+                    .filter((element: any) => {
+                        const elementDate = formatDate(element.heure);
+
+                        if (elementDate === date) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    })
+                    .map((element: any, index: any) => {
+                        const time = formatDate(element.heure);
+
+                        return {
+                            id: index + 1,
+                            nom: element.patient.nom,
+                            prenom: element.patient.prenom,
+                            sexe: element.patient.sexe,
+                            telephone: element.patient.tel,
+                            motif: element.motif,
+                            heure: time,
+                        };
+                    }),
+            );
+        });
+    }, [date]);
 
     const columns: GridColDef[] = [
         { field: "nom", headerName: "Nom", width: 200, type: "date" },
         { field: "prenom", headerName: "Prenom", width: 230 },
         { field: "telephone", headerName: "Telephone", width: 200 },
-        { field: "age", headerName: "Age", width: 110 },
         { field: "motif", headerName: "Motif", width: 150 },
         { field: "heure", headerName: "Heure", width: 150 },
         {
@@ -55,46 +67,65 @@ export default function Rdvs() {
             },
         },
     ];
+
     return (
-        <div className={style.Rdvs}>
-            <div className={style.title}>Rendez-vous</div>
-            <main>
-                <div className={style.add_rdv}>
-                    <img src={newRdvIcon} alt="" />
-                    <h3>Créer Rendez-vous</h3>
-                </div>
-                <div className={style.table}>
-                    <h2 className={style.nbRdv}>
-                        2 Rendez-vous pour le{" "}
-                        <span className={style.blueDate}>02/06/2022</span>
-                    </h2>
-                    <DataGrid
-                        columns={columns}
-                        rows={rows}
-                        hideFooterSelectedRowCount
-                        disableSelectionOnClick
-                        pageSize={11}
-                        sx={{
-                            border: "none",
-                            fontStyle: "normal",
-                            fontWeight: "500",
-                            fontSize: "16px",
-                            lineHeight: "19px",
-                            color: "#455560",
-                            "& .MuiDataGrid-row:hover .invis": {
-                                visibility: "visible",
-                                backgroundColor: "#FAFAFB",
-                            },
-                            "& .MuiDataGrid-row:hover": {
-                                backgroundColor: "#FAFAFB",
-                            },
-                            "&.MuiDataGrid-root .MuiDataGrid-cell:focus": {
-                                outline: "none",
-                            },
-                        }}
-                    />
-                </div>
-            </main>
-        </div>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <div className={style.Rdvs}>
+                <div className={style.title}>Rendez-vous</div>
+                <main>
+                    <div className={style.add_rdv}>
+                        <img src={newRdvIcon} alt="" />
+                        <h3>Créer Rendez-vous</h3>
+                    </div>
+                    <div className={style.table}>
+                        <div className={style.date}>
+                            <h2 className={style.nbRdv}>
+                                {data && data.length} Rendez-vous pour le{" "}
+                                <span className={style.blueDate}>
+                                    {formatDate(date)}
+                                </span>
+                            </h2>
+                            <DesktopDatePicker
+                                label="Date"
+                                inputFormat="yyyy/MM/DD"
+                                value={date}
+                                onChange={(newDate) => {
+                                    setDate(formatDate(newDate!));
+                                }}
+                                renderInput={(params) => (
+                                    <TextField {...params} />
+                                )}
+                            />
+                        </div>
+
+                        <DataGrid
+                            columns={columns}
+                            rows={data}
+                            hideFooterSelectedRowCount
+                            disableSelectionOnClick
+                            pageSize={11}
+                            sx={{
+                                border: "none",
+                                fontStyle: "normal",
+                                fontWeight: "500",
+                                fontSize: "16px",
+                                lineHeight: "19px",
+                                color: "#455560",
+                                "& .MuiDataGrid-row:hover .invis": {
+                                    visibility: "visible",
+                                    backgroundColor: "#FAFAFB",
+                                },
+                                "& .MuiDataGrid-row:hover": {
+                                    backgroundColor: "#FAFAFB",
+                                },
+                                "&.MuiDataGrid-root .MuiDataGrid-cell:focus": {
+                                    outline: "none",
+                                },
+                            }}
+                        />
+                    </div>
+                </main>
+            </div>
+        </LocalizationProvider>
     );
 }
