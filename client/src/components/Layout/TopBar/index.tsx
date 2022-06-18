ed1d6e5895a26427a09444176b0b20e05e22b88a
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-18 16:11:37 +0100

    global auto complete

--- client/src/components/Layout/TopBar/index.tsx
+++ client/src/components/Layout/TopBar/index.tsx
@@ -6,7 +6,8 @@ import { logout, selectUser } from "../../../features/userSlice";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { load } from "../../../features/patientSlice";
+import { load, selectPatients } from "../../../features/patientSlice";
+import { Autocomplete, TextField } from "@mui/material";
 
 export default function TopBar() {
     const dispatch = useDispatch();
@@ -14,6 +15,15 @@ export default function TopBar() {
 
     const [active, setActive] = useState(false);
     const user = useSelector(selectUser);
+    const [options, setOptions] = useState(null);
+    const [search, setSearch] = useState(null);
+
+    // const options = patients.map((patient: any) => {
+    //     return {
+    //         label: `${patient.nom} ${patient.prenom}`,
+    //         id: patient.id,
+    //     };
+    // });
 
     const handleLogout = async (e: any) => {
         e.preventDefault();
@@ -44,16 +54,68 @@ export default function TopBar() {
                     patients: patients,
                 }),
             );
-        }, 500);
+
+            const option = patients.map((patient: any) => {
+                return {
+                    label: `${patient.nom} ${patient.prenom}`,
+                    id: patient.id,
+                };
+            });
+
+            setOptions(option);
+        }, 50);
     }, []);
 
     const name = `${user.prenom} ${user.nom}`;
 
     return (
         <div className={style.TopBar}>
-            <div className={style.search}>
-                <input type="text" placeholder="Rechercher..." />
+            {/* <div className={style.search}>
+                {options && (
+                    <Autocomplete
+                        options={options}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="standard"
+                                // InputProps={{
+                                //     disableUnderline: true, // <== added this
+                                // }}
+                                fullWidth
+                                sx={{ width: "260px" }}
+                                hiddenLabel
+                            />
+                        )}
+                        openOnFocus
+                    />
+                )}
                 <img src={searchIcon} alt="" />
+            </div> */}
+
+            <div className={style.newSearch}>
+                {options && (
+                    <Autocomplete
+                        options={options}
+                        renderInput={(params) => (
+                            <TextField
+                                placeholder="Rechercher..."
+                                {...params}
+                                variant="outlined"
+                                // InputProps={{
+                                //     disableUnderline: true, // <== added this
+                                // }}
+                                fullWidth
+                                sx={{ width: "300px" }}
+                                hiddenLabel
+                            />
+                        )}
+                        value={search}
+                        onChange={(e, value: any) => {
+                            navigate(`/Patients/${value.id}`);
+                            setSearch(null);
+                        }}
+                    />
+                )}
             </div>
 
             <div className={style.logout}>
