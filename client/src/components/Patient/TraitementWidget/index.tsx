05fd325466a036c59e7f758872b33cb3b09f3053
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-06-18 14:33:31 +0100

    Traitement widget now renders

--- client/src/components/Patient/TraitementWidget/index.tsx
+++ client/src/components/Patient/TraitementWidget/index.tsx
@@ -1,14 +1,30 @@
+import moment from "moment";
 import style from "./style.module.css";
 
-export default function TraitementWidget() {
+export default function TraitementWidget({ traitements }: any) {
+    let traitementsRender;
+    if (traitements.length > 0) {
+        traitementsRender = traitements.map((traitement: any) => {
+            if (traitement.actif) {
+                return (
+                    <>
+                        <h2>{traitement.nom}</h2> <span>du</span>{" "}
+                        <h2>{moment(traitement.debut).format("yyyy/MM/DD")}</h2>{" "}
+                        <span>au</span>{" "}
+                        <h2>{moment(traitement.fin).format("yyyy/MM/DD")}</h2>
+                    </>
+                );
+            }
+        });
+    } else {
+        traitementsRender = "Aucun traitement en cours";
+    }
     return (
         <div className={style.TraitementWidget}>
             <h1>Traitement</h1>
             <div className={style.traitement}>
-                <h2>Retrofilazol</h2> <span>du</span> <h2>22/10/2000</h2>{" "}
-                <span>au</span> <h2>22/10/2000</h2>
+                <h2>{traitementsRender}</h2>
             </div>
-
         </div>
     );
 }
